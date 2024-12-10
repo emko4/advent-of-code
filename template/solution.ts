4dd6b395ac768b3116873631380b77a42454bdce
Martin Kadlec <martin.kadlec@erstegroup.com>
2024-12-10 13:07:40 +0100

    Update template

--- template/solution.ts
+++ template/solution.ts
@@ -2,7 +2,7 @@ export const processData = (data: Buffer): any => {
     return data.toString();
 };
 
-export const solution = (input: any): any => {
+export const solution = (input: any): number => {
     console.log('Input: ', input);
-    return 'Result';
+    return 0;
 };
