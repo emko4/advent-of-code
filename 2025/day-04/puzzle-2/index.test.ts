d99cc9e7918edce1a95bdc5ae69fab2a969c3361
Martin Kadlec <martin.kadlec@erstegroup.com>
2025-12-04 09:09:37 +0100

    Add 2025 day 4

---
+++ 2025/day-04/puzzle-2/index.test.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+import { processData, solution } from './solution';
+
+const exampleData = fs.readFileSync(__dirname + '/example-input.txt');
+const exampleResult = fs.readFileSync(__dirname + '/example-result.txt');
+
+describe('Puzzle 2 from day 04 of the year 2025', () => {
+    it('returns same result as AoC example result', () => {
+        const input = processData(exampleData);
+        const result = exampleResult.toString();
+
+        expect(solution(input).toString()).toStrictEqual(result);
+    });
+});
---
+++ 2025/day-04/puzzle-2/solution.ts
@@ -0,0 +1,63 @@
+type Item = '.' | '@';
+type Map = Item[][];
+
+export const processData = (data: Buffer): Map => {
+    return data
+        .toString()
+        .split('\n')
+        .map((line) => line.split('') as Item[]);
+};
+
+const MAX_PAPER_ROLLS = 3;
+
+const checkItem = (map: Map, x: number, y: number): boolean => {
+    if (map[x][y] !== '@') return false;
+
+    let paperRollsCount = 0;
+
+    for (let i = x - 1; i <= x + 1; i += 1) {
+        if (i < 0 || i >= map.length) continue;
+
+        for (let j = y - 1; j <= y + 1; j += 1) {
+            if (j < 0 || j >= map[0].length) continue;
+            if (x === i && y === j) continue;
+
+            if (map[i][j] === '@') paperRollsCount += 1;
+        }
+    }
+
+    return paperRollsCount <= MAX_PAPER_ROLLS;
+};
+
+let mutableMap: Map;
+
+const checkMap = (): number => {
+    let result = 0;
+
+    for (let x = 0; x < mutableMap.length; x += 1) {
+        for (let y = 0; y < mutableMap[0].length; y += 1) {
+            if (checkItem(mutableMap, x, y)) {
+                mutableMap[x][y] = '.';
+                result += 1;
+            }
+        }
+    }
+
+    return result;
+};
+
+export const solution = (map: Map): number => {
+    mutableMap = map.map((row) => [...row]);
+
+    let result = 0;
+
+    while (true) {
+        const checkResult = checkMap();
+
+        if (checkResult === 0) break;
+
+        result += checkResult;
+    }
+
+    return result;
+};
